c43fedf5a7ffd0979e634a9a5b5ab13acd77fb6d
James McCann <jmccnz@gmail.com>
2012-08-18 01:49:40 +1200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rhs3::Application.config.secret_token = '44d1c2cb75b6cbde571f4e9d32e8cd5277ac7342cc7371b7d1cdfd48ad03c5fa151e803e510be0ad34656d1f3ee48b2d17d1951253935737feb03d7c4d71c580'
